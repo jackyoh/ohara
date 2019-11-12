440ce10b421ea8a2576e99a472fba2289b6b6fb5
jackyoh <jack@is-land.com.tw>
2019-11-12 18:26:23 +0800

    fix_3148 Added metric service url setting to Configurator

--- ohara-configurator/src/main/scala/com/island/ohara/configurator/ConfiguratorBuilder.scala
+++ ohara-configurator/src/main/scala/com/island/ohara/configurator/ConfiguratorBuilder.scala
@@ -46,6 +46,7 @@ class ConfiguratorBuilder private[configurator] extends Builder[Configurator] {
   private[this] var store: DataStore = _
   private[this] var serviceCollie: ServiceCollie = _
   private[this] var k8sClient: K8SClient = _
+  private[this] var metricsServiceURL: String = _
   private[this] var k8sNamespace: String = K8SClient.NAMESPACE_DEFAULT_VALUE
 
   @Optional("default is random folder")
@@ -381,6 +382,7 @@ class ConfiguratorBuilder private[configurator] extends Builder[Configurator] {
   def k8sClient(k8sClient: K8SClient): ConfiguratorBuilder = doOrReleaseObjects {
     if (this.k8sClient != null) throw new IllegalArgumentException(alreadyExistMessage("k8sClient"))
     if (this.serviceCollie != null) throw new IllegalArgumentException(alreadyExistMessage("serviceCollie"))
+    if (this.metricsServiceURL != null) this.k8sClient.k8sMetricsAPIServerURL(metricsServiceURL)
     this.k8sClient = Objects.requireNonNull(k8sClient)
     this
   }
@@ -393,6 +395,11 @@ class ConfiguratorBuilder private[configurator] extends Builder[Configurator] {
   @Optional("default value is default")
   private[configurator] def k8sNamespace(namespace: String): ConfiguratorBuilder = {
     this.k8sNamespace = namespace
+    this
+  }
+
+  private[configurator] def k8sMetricsServiceURL(metricsServiceURL: String): ConfiguratorBuilder = {
+    this.metricsServiceURL = metricsServiceURL
     this
   }
 
