e9b0d33f79258848dcfa27451c94115a8e6bf2a6
jackyoh <jack@is-land.com.tw>
2019-12-02 18:16:18 +0800

    fix_3030 Modify shell script to deploy hdfs container

--- ohara-it/docker/hdfs-container.sh
+++ ohara-it/docker/hdfs-container.sh
@@ -15,36 +15,21 @@
 # limitations under the License.
 #
 
+usage="USAGE: $0 [start|stop|--help] arg1 arg2 ..."
 if [ $# -lt 1 ];
 then
-  echo "USAGE: $0 [start-all|start-namenode|start-datanode|stop-all|stop-namenode|stop-datanode]" arg1 arg2 ...
+  echo $usage
   exit 1
 fi
 
 COMMAND=$1
 case $COMMAND in
-  start-all)
-    start_all="true"
+  start)
+    start="true"
     shift
     ;;
-  start-namenode)
-    start_namenode="true"
-    shift
-    ;;
-  start-datanode)
-    start_datanode="true"
-    shift
-    ;;
-  stop-all)
-    stop_all="true"
-    shift
-    ;;
-  stop-namenode)
-    stop_namenode="true"
-    shift
-    ;;
-  stop-datanode)
-    stop_datanode="true"
+  stop)
+    stop="true"
     shift
     ;;
   --help)
@@ -52,29 +37,38 @@ case $COMMAND in
     shift
     ;;
   *)
-    echo "USAGE: $0 [start-all|start-namenode|start-datanode|stop-all|stop-namenode|stop-datanode|--help]" arg1 arg2 ...
+    echo $usage
     exit 1
     ;;
 esac
 
-while getopts n:h: option
+while getopts n:s:u: option
 do
  case "${option}"
  in
  n) nameNode=${OPTARG};;
+ s) dataNodes=${OPTARG};;
+ u) userName=${OPTARG};;
  esac
 done
 
 if [ "${help}" == "true" ];
 then
-  echo "USAGE: $0 [start-all|start-namenode|start-datanode|stop-all|stop-namenode|stop-datanode]" arg1 arg2 ...
+  echo $usage
   echo "Argument             Description"
   echo "--------             -----------"
   echo "-n                   Set HDFS namenode hostname and port to start the datanode. example: -n host1:9000"
+  echo "-s                   Set HDFS datanode hostname list. example: -s host1,host2,host3"
+  echo "-u                   Set ssh user name to remote deploy datanode"
   exit 1
 fi
 
-if [ -z "${nameNode}" ] && ([ "$start_all" == "true" ] || [ "$start_datanode" == "true" ]);
+if [[ -z "${userName}" ]];
+then
+  userName="ohara"
+fi
+
+if [[ -z "${nameNode}" ]] && [[ "$start" == "true" ]];
 then
   echo 'Please setting the -n ${NAMENODE_HOST_AND_PORT} argument'
   exit 1
@@ -84,42 +78,30 @@ nameNodeImageName="oharastream/ohara:hdfs-namenode"
 dataNodeImageName="oharastream/ohara:hdfs-datanode"
 
 nameNodeContainerName="namenode"
-dataNodeContainerName="datanode_${HOSTNAME}"
+dataNodeContainerName="datanode"
 
-if [ "$start_all" == "true" ];
+IFS=","
+if [ "$start" == "true" ];
 then
-  echo "Start HDFS container"
+  echo "Starting HDFS container"
+  echo "Starting ${HOSTNAME} node namenode......"
   docker run -d -it --name ${nameNodeContainerName} --net host ${nameNodeImageName}
-  docker run -d -it --name ${dataNodeContainerName} --env HADOOP_NAMENODE=${nameNode} --net host ${dataNodeImageName}
+
+  for dataNode in $dataNodes;
+  do
+    echo "Starting ${dataNode} node datanode......"
+    ssh ${userName}@$dataNode docker run -d -it --name ${dataNodeContainerName} --env HADOOP_NAMENODE=${nameNode} --net host ${dataNodeImageName}
+  done
 fi
 
-if [ "$start_namenode" == "true" ];
+if [ "$stop" == "true" ];
 then
-  echo "Start HDFS Namenode container"
-  docker run -d -it --name ${nameNodeContainerName} --net host ${nameNodeImageName}
-fi
-
-if [ "$start_datanode" == "true" ];
-then
-  echo "Start HDFS Datanode container"
-  docker run -d -it --name ${dataNodeContainerName} --env HADOOP_NAMENODE=${nameNode} --net host ${dataNodeImageName}
-fi
-
-if [ "$stop_all" == "true" ];
-then
-  echo "Stop HDFS container"
-  docker rm -f ${nameNodeContainerName}
-  docker rm -f ${dataNodeContainerName}
-fi
-
-if [ "$stop_namenode" == "true" ];
-then
-  echo "Stop HDFS Namenode container"
-  docker rm -f ${nameNodeContainerName}
-fi
-
-if [ "$stop_datanode" == "true" ];
-then
-  echo "Stop HDFS Datanode container"
-  docker rm -f ${dataNodeContainerName}
+    echo "Stoping HDFS container"
+    for dataNode in $dataNodes;
+    do
+      echo "Stoping ${dataNode} node datanode......"
+      ssh ${userName}@$dataNode docker rm -f ${dataNodeContainerName}
+    done
+    echo "Stoping ${HOSTNAME} node namenode......"
+    docker rm -f ${nameNodeContainerName}
 fi
