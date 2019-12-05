14daf983d14a92096dfc7c11e803639c0769d07c
jackyoh <jack@is-land.com.tw>
2019-12-05 13:52:34 +0800

    fix_3030 Added test class for the jdbc

---
+++ ohara-it/src/test/scala/com/island/ohara/it/performance/TestPerformance4Jdbc.scala
@@ -0,0 +1,16 @@
+package com.island.ohara.it.performance
+
+import com.island.ohara.it.category.PerformanceGroup
+import org.junit.Test
+import org.junit.experimental.categories.Category
+
+@Category(Array(classOf[PerformanceGroup]))
+class TestPerformance4Jdbc extends BasicTestPerformance {
+
+  @Test
+  def test(): Unit = {
+
+  }
+
+
+}
