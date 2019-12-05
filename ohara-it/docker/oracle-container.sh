5e9a232cea8eaa52e44a21c3f61881226391b8d6
jackyoh <jack@is-land.com.tw>
2019-12-05 09:32:00 +0800

    fix_3030 Modify shell script and add README.md file for deploy oracle database container

--- ohara-it/docker/oracle-container.sh
+++ ohara-it/docker/oracle-container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+usage="USAGE: $0 [start|stop|--help] -u"' ${USER_NAME} -p ${PASSWORD} ....'
 
 if [ $# -lt 1 ];
 then
-  echo "USAGE: $0 [start|stop] -u"' ${USER_NAME} -p ${PASSWORD} ....'
+  echo $usage
   exit 1
 fi
 
@@ -16,17 +17,33 @@ case $COMMAND in
     stop="true"
     shift
     ;;
+  --help)
+    help="true"
+    shift
+    ;;
   *)
-    echo "USAGE: $0 [start|stop] -u"' ${USER_NAME} -p ${PASSWORD} ....'
+    echo $usage
     exit 1
     ;;
 esac
 
-sid="ORACLE_SID"
-domain="localdomain"
-port="1521"
+if [ "${help}" == "true" ];
+then
+  echo $usage
+  echo "Argument             Description"
+  echo "--------             -----------"
+  echo "--user               Set oracle database user name"
+  echo "--password           Set oracle database password"
+  echo "--port               Set connection port for client"
+  echo "--sid                Set connection sid"
+  exit 1
+fi
 
-ARGUMENT_LIST=("user" "password" "port" "sid" "domain")
+sid="xe"
+port="1521"
+containerName="oracle-benchmark-test"
+
+ARGUMENT_LIST=("user" "password" "port" "sid")
 
 opts=$(getopt \
     --longoptions "$(printf "%s:," "${ARGUMENT_LIST[@]}")" \
@@ -54,35 +71,36 @@ while [[ $# -gt 0 ]]; do
       sid=$2
       shift 2
       ;;
-    --domain)
-      domain=$2
-      shift 2
-      ;;
-    --help)
-      domain=$2
-      shift 2
-      ;;
     *)
       break
       ;;
   esac
 done
 
-if [[ -z ${user} ]];
+if [[ -z ${user} ]] && [[ "${start}" == "true" ]];
 then
   echo 'Please set the --user ${USER_NAME} argument'
   exit 1
 fi
 
-if [[ -z ${password} ]];
+if [[ -z ${password} ]] && [[ "${start}" == "true" ]];
 then
   echo 'Please set the --password ${PASSWORD} argument'
   exit 1
 fi
 
-echo "user=$user"
-echo "password=$password"
-echo "port=$port"
-echo "sid=$sid"
-echo "domain=$domain"
+if [[ "${start}" == "true" ]];
+then
+  echo "Starting oracle database container"
+  echo "Port is ${port}"
+  docker run -d -i --name ${containerName} --restart=always -p ${port}:1521 --env DB_SID=${sid} store/oracle/database-enterprise:12.2.0.1
+  sleep 3m
+  docker exec -i $containerName bash -c "source /home/oracle/.bashrc;echo -e 'alter session set \"_ORACLE_SCRIPT\"=true;\ncreate user ${user} identified by ${password};\nGRANT CONNECT, RESOURCE, DBA TO ${user};'|sqlplus sys/Oradoc_db1@${sid} as sysdba"
+  echo "Start oracle database complete. User name is ${user}"
+fi
 
+if [[ "${stop}" == "true" ]];
+then
+  echo "Stoping oracle database container"
+  docker rm -f $containerName
+fi
