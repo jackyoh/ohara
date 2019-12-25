63e0378c4bd5f1a9978addf2cfc137afd56160e3
jackyoh <jack@is-land.com.tw>
2019-12-25 13:53:39 +0800

    fix_3030 Added performance group to TestPerformance4FtpSink test class

--- ohara-it/src/test/scala/com/island/ohara/it/performance/TestPerformance4FtpSink.scala
+++ ohara-it/src/test/scala/com/island/ohara/it/performance/TestPerformance4FtpSink.scala
@@ -19,10 +19,13 @@ package com.island.ohara.it.performance
 import com.island.ohara.common.setting.{ConnectorKey, TopicKey}
 import com.island.ohara.common.util.CommonUtils
 import com.island.ohara.connector.ftp.FtpSink
+import com.island.ohara.it.category.PerformanceGroup
 import com.island.ohara.kafka.connector.csv.CsvConnectorDefinitions
 import spray.json.JsString
 import org.junit.Test
+import org.junit.experimental.categories.Category
 
+@Category(Array(classOf[PerformanceGroup]))
 class TestPerformance4FtpSink extends BasicTestPerformance4Ftp {
   private[this] val connectorKey: ConnectorKey = ConnectorKey.of("benchmark", CommonUtils.randomString(5))
   private[this] val topicKey: TopicKey         = TopicKey.of("benchmark", CommonUtils.randomString(5))
