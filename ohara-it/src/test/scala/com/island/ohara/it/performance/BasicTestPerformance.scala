81686904a6f37a4d1c65fcf7015a52c75458b7cc
jackyoh <jack@is-land.com.tw>
2019-12-30 16:17:10 +0800

    fix_3030 Modify stop connector await

--- ohara-it/src/test/scala/com/island/ohara/it/performance/BasicTestPerformance.scala
+++ ohara-it/src/test/scala/com/island/ohara/it/performance/BasicTestPerformance.scala
@@ -251,7 +251,15 @@ abstract class BasicTestPerformance extends WithRemoteWorkers {
 
     // record topic meters
     recordCsv(path("topic"), result(topicApi.list()).flatMap(_.metrics.meters))
-    if (this.connectorKey != null) result(connectorApi.stop(this.connectorKey))
+    if (this.connectorKey != null)
+      await(
+        () => {
+          result(connectorApi.stop(connectorKey))
+          true
+        },
+        true
+      )
+
     afterStoppingConnector()
   }
 
