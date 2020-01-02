f09c4e5dfb51f5e4bc11f38b2436856b63fd74a0
jackyoh <jack@is-land.com.tw>
2020-01-02 10:26:47 +0800

    fix_3030 Modify start-service.sh shell to start hdfs, ftp, samaba and oracle service

--- ohara-it/docker/start-service.sh
+++ ohara-it/docker/start-service.sh
@@ -6,3 +6,51 @@ then
   echo $usage
   exit 1
 fi
+
+COMMAND=$1
+case $COMMAND in
+  hdfs)
+    hdfs="true"
+    shift
+    ;;
+  ftp)
+    ftp="true"
+    shift
+    ;;
+  samba)
+    samba="true"
+    shift
+    ;;
+  oracle)
+    oracle="true"
+    shift
+    ;;
+  *)
+    echo $usage
+    exit 1
+    ;;
+esac
+
+if [[ "${hdfs}" == "true" ]];
+then
+   echo "Start HDFS service"
+   bash hdfs-container.sh start $@
+fi
+
+if [[ "${ftp}" == "true" ]];
+then
+  echo "Start Oracle service"
+  bash ftp-container.sh start $@
+fi
+
+if [[ "${samba}" == "true" ]];
+then
+  echo "Start Samba service"
+  bash samba-container.sh start $@
+fi
+
+if [[ "${oracle}" == "true" ]];
+then
+  echo "Start Oracle service"
+  bash oracle-container.sh start $@
+fi
