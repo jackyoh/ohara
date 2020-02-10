5c7ae5483a75f9f4a0e458977d1ccfec438d8508
jackyoh <jack@is-land.com.tw>
2020-02-10 15:52:31 +0800

    Modify shell script

--- ohara-it/docker/oracle-container.sh
+++ ohara-it/docker/oracle-container.sh
@@ -153,8 +153,9 @@ then
   done
 
   sleep 1m # Final confirm the Oracle database container is running and setup complete
-  ssh ohara@${host} << EOF
-    docker exec -i ${containerName} bash -c "source /home/oracle/.bashrc;echo -e 'alter session set \"_ORACLE_SCRIPT\"=true;\ncreate user ${user} identified by ${password};\nGRANT CONNECT, RESOURCE, DBA TO ${user};'|sqlplus sys/Oradoc_db1@${sid} as sysdba"
+
+ssh ohara@${host} << EOF
+docker exec -i ${containerName} bash -c "source /home/oracle/.bashrc;echo -e 'alter session set \"_ORACLE_SCRIPT\"=true;\ncreate user ${user} identified by ${password};\nGRANT CONNECT, RESOURCE, DBA TO ${user};'|sqlplus sys/Oradoc_db1@${sid} as sysdba"
 EOF # EOF key word can't indentation in if statement
 
   echo "Start oracle database complete. User name is ${user}"
