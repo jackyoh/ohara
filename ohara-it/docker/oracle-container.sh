e1d83a6a70ad56d1c31145ea8974101829c3b9a2
jackyoh <jack@is-land.com.tw>
2020-02-10 13:53:25 +0800

    Modify shell script

--- ohara-it/docker/oracle-container.sh
+++ ohara-it/docker/oracle-container.sh
@@ -148,7 +148,7 @@ done
 
 res=false
 timeoutCount=0
-while [[ ! res ]]
+while [[ $res == false ]]
 do
 ssh ohara@${host} << EOF
 docker exec -i ${containerName} bash -c "source /home/oracle/.bashrc;echo -e 'alter session set \"_ORACLE_SCRIPT\"=true;\ncreate user ${user} identified by ${password};\nGRANT CONNECT, RESOURCE, DBA TO ${user};'|sqlplus sys/Oradoc_db1@${sid} as sysdba"
