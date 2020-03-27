6a2877241ab57b9d41b5171881036d363c387851
jack <jack@is-land.com.tw>
2020-03-27 12:40:55 +0800

    fix_4439 Modify node list to get hostname

--- ohara-it/src/test/scala/oharastream/ohara/it/WithRemoteConfigurator.scala
+++ ohara-it/src/test/scala/oharastream/ohara/it/WithRemoteConfigurator.scala
@@ -102,12 +102,9 @@ abstract class WithRemoteConfigurator(paltform: PaltformModeInfo) extends Integr
     // wait for configurator
     TimeUnit.SECONDS.sleep(20)
 
-    val nodeApi      = NodeApi.access.hostname(configuratorHostname).port(configuratorPort)
-    val hostNameList = result(nodeApi.list()).map(_.hostname)
-    println(s"HOST NAME LIST:${hostNameList}")
-
+    val nodeApi = NodeApi.access.hostname(configuratorHostname).port(configuratorPort)
     nodes.foreach { node =>
-      println(s"DEBUG ${node.hostname}")
+      val hostNameList = result(nodeApi.list()).map(_.hostname)
       if (!hostNameList.contains(node.hostname)) {
         result(
           nodeApi.request
