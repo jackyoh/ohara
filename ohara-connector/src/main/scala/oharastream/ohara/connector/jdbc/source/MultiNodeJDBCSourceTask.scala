e7290510ce58926eed1b952426bfffe09e4a1165
jackyoh <jack@is-land.com.tw>
2020-05-26 15:44:55 +0800

    Modify current time function

--- ohara-connector/src/main/scala/oharastream/ohara/connector/jdbc/source/MultiNodeJDBCSourceTask.scala
+++ ohara-connector/src/main/scala/oharastream/ohara/connector/jdbc/source/MultiNodeJDBCSourceTask.scala
@@ -14,6 +14,7 @@ class MultiNodeJDBCSourceTask extends RowSourceTask {
   private[this] var jdbcSourceConnectorConfig: JDBCSourceConnectorConfig = _
   private[this] var client: DatabaseClient                               = _
   private[this] var testCount: Int                                       = 0
+  private[this] var dbProduct: String = _
 
   override protected def run(settings: TaskSetting): Unit = {
     jdbcSourceConnectorConfig = JDBCSourceConnectorConfig(settings)
@@ -22,6 +23,7 @@ class MultiNodeJDBCSourceTask extends RowSourceTask {
       .user(jdbcSourceConnectorConfig.dbUserName)
       .password(jdbcSourceConnectorConfig.dbPassword)
       .build
+    dbProduct = client.connection.getMetaData.getDatabaseProductName
   }
 
   override protected def pollRecords(): util.List[RowSourceRecord] = {
@@ -74,8 +76,17 @@ class MultiNodeJDBCSourceTask extends RowSourceTask {
   }
 
   private[this] def overCurrentTimestamp(startTimestamp: Timestamp, stopTimestamp: Timestamp): Boolean = {
-    startTimestamp.getTime()
-    stopTimestamp.getTime()
-    false // TODO
+    val query = dbProduct.toLowerCase match {
+      case ORACLE_DB_NAME => "SELECT CURRENT_TIMESTAMP FROM dual"
+      case _              => "SELECT CURRENT_TIMESTAMP;"
+    }
+    val stmt = client.connection.createStatement()
+    try {
+      val rs = stmt.executeQuery(query)
+      try {
+        val currentTimestamp = if (rs.next()) rs.getTimestamp(1) else new Timestamp(0)
+        (startTimestamp.getTime > currentTimestamp.getTime && stopTimestamp.getTime > currentTimestamp.getTime)
+      } finally Releasable.close(rs)
+    } finally Releasable.close(stmt)
   }
 }
