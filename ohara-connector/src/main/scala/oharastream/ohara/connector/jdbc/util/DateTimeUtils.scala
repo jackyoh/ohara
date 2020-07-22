a80603503ec6fc9030c0a75e0160951df777eb24
jackyoh <jack@is-land.com.tw>
2020-07-22 18:11:56 +0800

    for timezone test

--- ohara-connector/src/main/scala/oharastream/ohara/connector/jdbc/util/DateTimeUtils.scala
+++ ohara-connector/src/main/scala/oharastream/ohara/connector/jdbc/util/DateTimeUtils.scala
@@ -19,7 +19,7 @@ package oharastream.ohara.connector.jdbc.util
 import java.util.{Calendar, TimeZone}
 
 object DateTimeUtils {
-  private val timeZoneID: String = System.getProperty("user.timezone")
+  private val timeZoneID: String = "Asia/Taipei"
   private val UTC: TimeZone      = TimeZone.getTimeZone(timeZoneID)
 
   val CALENDAR: Calendar = Calendar.getInstance(UTC)
