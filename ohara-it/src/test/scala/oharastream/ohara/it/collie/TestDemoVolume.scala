0bc566ee8ec767dc54f51a92b240b6275072d723
jack <jack@is-land.com.tw>
2020-08-17 17:23:36 +0800

    Added license header and add port variable

--- ohara-it/src/test/scala/oharastream/ohara/it/collie/TestDemoVolume.scala
+++ ohara-it/src/test/scala/oharastream/ohara/it/collie/TestDemoVolume.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 is-land
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package oharastream.ohara.it.collie
 
 import java.time.Duration
@@ -12,7 +28,8 @@ import org.junit.Test
 
 class TestDemoVolume extends OharaTest {
   private[this] val topicKey: TopicKey      = TopicKey.of("group", "topic1")
-  private[this] val connectionProps: String = "ohara-jenkins-it-00:36876"
+  private[this] val brokerPort: Int = 40540
+  private[this] val connectionProps: String = s"ohara-jenkins-it-00:$brokerPort"
 
   @Test
   def testProduce(): Unit = {
