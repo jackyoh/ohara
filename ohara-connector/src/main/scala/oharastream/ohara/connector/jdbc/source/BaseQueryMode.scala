68842a51ca041fb30e09ec602ac93cf61eb28420
jackyoh <jack@is-land.com.tw>
2020-09-08 09:17:58 +0800

    fix_5434 Refactor JDBC source task for the timestamp mode

---
+++ ohara-connector/src/main/scala/oharastream/ohara/connector/jdbc/source/BaseQueryMode.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 is-land
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package oharastream.ohara.connector.jdbc.source
+
+import java.sql.Timestamp
+
+import oharastream.ohara.kafka.connector.RowSourceRecord
+
+trait BaseQueryMode {
+  protected[source] def queryData(startTimestamp: Timestamp, stopTimestamp: Timestamp): Seq[RowSourceRecord]
+
+  /**
+    * The start timestamp and stop timestamp range can't change.
+    * @Param key timestamp key
+    * @param startTimestamp start timestamp
+    * @param stopTimestamp stop timestamp
+    * @return true or false
+    */
+  protected[source] def isCompleted(key: String, startTimestamp: Timestamp, stopTimestamp: Timestamp): Boolean
+}
