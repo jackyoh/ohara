37ac43652b5e9757d2a60fe7c7eee7cf193a5b8a
jackyoh <jack@is-land.com.tw>
2020-10-22 16:14:50 +0800

    fix_5693 Added volume for the container

--- ohara-agent/src/main/scala/oharastream/ohara/agent/RemoteFolderHandler.scala
+++ ohara-agent/src/main/scala/oharastream/ohara/agent/RemoteFolderHandler.scala
@@ -179,13 +179,9 @@ object RemoteFolderHandler {
     private[this] def agent[T](
       hostname: String
     )(f: Agent => T)(implicit executionContext: ExecutionContext): Future[T] = {
-      println(s"HostName: $hostname")
       dataCollie
         .value[Node](hostname)
         .map { node =>
-          println(
-            s"NodeHostname: ${node.hostname}, NodeUser: ${node.user}, NodePassword: ${node.password} and NodePort: ${node.port}"
-          )
           Agent.builder
             .hostname(node.hostname)
             .user(node.user)
