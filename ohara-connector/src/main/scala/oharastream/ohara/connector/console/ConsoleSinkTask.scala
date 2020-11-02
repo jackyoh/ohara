2346f89430cebaf08c5fdeadcf19d6106ab1bb86
jackyoh <jack@is-land.com.tw>
2020-11-02 16:27:09 +0800

    fix_5738 Added columns for the console sink connector

--- ohara-connector/src/main/scala/oharastream/ohara/connector/console/ConsoleSinkTask.scala
+++ ohara-connector/src/main/scala/oharastream/ohara/connector/console/ConsoleSinkTask.scala
@@ -23,12 +23,14 @@ import oharastream.ohara.common.annotations.VisibleForTesting
 import oharastream.ohara.common.util.CommonUtils
 import oharastream.ohara.kafka.connector.{RowSinkRecord, RowSinkTask, TaskSetting}
 import com.typesafe.scalalogging.Logger
+import oharastream.ohara.common.data.{Cell, Column, DataType, Row}
 
 import scala.concurrent.duration.Duration
 import scala.jdk.CollectionConverters._
 
 class ConsoleSinkTask extends RowSinkTask {
-  private[this] val LOG = Logger(classOf[ConsoleSinkTask])
+  private[this] val LOG                  = Logger(classOf[ConsoleSinkTask])
+  private[this] var columns: Seq[Column] = _
   @VisibleForTesting
   private[console] var freq: Duration = CONSOLE_FREQUENCE_DEFAULT
   @VisibleForTesting
@@ -36,6 +38,7 @@ class ConsoleSinkTask extends RowSinkTask {
   @VisibleForTesting
   private[console] var lastLog: Long = -1
   override protected def run(config: TaskSetting): Unit = {
+    this.columns = config.columns().asScala.toSeq
     divider = config.stringOption(CONSOLE_ROW_DIVIDER).orElse(CONSOLE_ROW_DIVIDER_DEFAULT)
     freq = Duration(
       config
@@ -50,10 +53,60 @@ class ConsoleSinkTask extends RowSinkTask {
     // do nothing
   }
 
-  override protected def putRecords(records: util.List[RowSinkRecord]): Unit =
+  override protected def putRecords(records: util.List[RowSinkRecord]): Unit = {
     if (!records.isEmpty && (lastLog == -1 || CommonUtils.current() - lastLog >= freq.toMillis)) {
       try {
-        LOG.info(records.asScala.map(_.row()).mkString(divider))
+        LOG.info(
+          records.asScala
+            .map {
+              if (columns.nonEmpty) convertToValue(_, columns)
+              else _.row()
+            }
+            .mkString(divider)
+        )
       } finally lastLog = CommonUtils.current()
     }
+  }
+
+  private[console] def convertToValue(record: RowSinkRecord, columns: Seq[Column]): Row = {
+    Row.of(
+      columns.sortBy(_.order()).map { c =>
+        val value = record.row().cell(c.name()).value()
+        checkDataType(c.dataType(), value)
+        Cell.of(c.newName(), value)
+      }: _*
+    )
+  }
+
+  private[console] def checkDataType(dataType: DataType, value: Any): Unit = {
+    dataType match {
+      case DataType.BOOLEAN =>
+        if (!value.isInstanceOf[java.lang.Boolean])
+          throw new IllegalArgumentException(s"$value value type is not java.lang.Boolean type")
+      case DataType.BYTE =>
+        if (!value.isInstanceOf[java.lang.Byte])
+          throw new IllegalArgumentException(s"$value value type is not java.lang.Byte type")
+      case DataType.BYTES =>
+        if (!value.isInstanceOf[Array[java.lang.Byte]]) throw new IllegalArgumentException(s"column is not Bytes type")
+      case DataType.SHORT =>
+        if (!value.isInstanceOf[java.lang.Short])
+          throw new IllegalArgumentException(s"$value value type is not java.lang.Short type")
+      case DataType.INT =>
+        if (!value.isInstanceOf[java.lang.Integer])
+          throw new IllegalArgumentException(s"$value value type is not java.lang.Integer type")
+      case DataType.LONG =>
+        if (!value.isInstanceOf[java.lang.Long])
+          throw new IllegalArgumentException(s"$value value type is not java.lang.Long type")
+      case DataType.FLOAT =>
+        if (!value.isInstanceOf[java.lang.Float])
+          throw new IllegalArgumentException(s"$value value type is not java.lang.Float type")
+      case DataType.DOUBLE =>
+        if (!value.isInstanceOf[java.lang.Double])
+          throw new IllegalArgumentException(s"$value value type is not java.lang.Double type")
+      case DataType.STRING =>
+        if (!value.isInstanceOf[java.lang.String])
+          throw new IllegalArgumentException(s"$value value type is not java.lang.String type")
+      case _ => throw new IllegalArgumentException("Data type setting error")
+    }
+  }
 }
