57d7db3ac4f8c52fd8bbaf8e6922994d4c907e28
jackyoh <jack@is-land.com.tw>
2020-11-03 18:24:22 +0800

    for test

--- ohara-agent/src/main/scala/oharastream/ohara/agent/ServiceCollie.scala
+++ ohara-agent/src/main/scala/oharastream/ohara/agent/ServiceCollie.scala
@@ -230,7 +230,7 @@ abstract class ServiceCollie extends Releasable {
             Some(
               ClusterVolume(
                 group = key.group(),
-                name = key.toPlain,
+                name = key.name().split("-").head,
                 path = volume.path,
                 driver = volume.driver,
                 state = None,
